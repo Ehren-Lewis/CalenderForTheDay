8a9b8f81ac656bf79eefc377f73698bf2cd28f73
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-14 17:02:26 -0500

    Fixed a bug where Object...includes would add the text value from 12PM to 2PM as well, committing to check to see if it is fixed in deployment as well

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,38 +1,38 @@
-const hour = 11;
+
+// Defining all of the time variables
+// as well as all the formatting for the date
 const date = new Date();
 const timeArray = ['AM', 'PM'];
-const newTimeArray = [9, 10, 11 , 12, 1, 2, 3, 4, 5]
-const finalTimerArray = [];
-
-
-
+const timeArrayUnformatted = [9, 10, 11 , 12, 1, 2, 3, 4, 5]
+const timeArrayFormatted = [];
 const months = ['January', 'February', 'March', 'April', 'May', 'June',
 'July', 'August', 'September', 'October', 'Novermber', 'December'];
-
 const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
 const todayKey = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
-
 const today = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 
 $("#currentDay").text(`${day[date.getDay(today)]} ${today}`);
 
 let hours = date.getHours(today);
 
+// Change from military time 
 if (hours > 12) {
     hours = hours - 12;
 }
-
-console.log(hours)
-
-for (let i = 0; i < newTimeArray.length; i++) {
-    if ( newTimeArray[i] < 6 || newTimeArray[i] == 12) {
-        finalTimerArray.push(`${newTimeArray[i]}PM`);
-    } else {
-        finalTimerArray.push(`${newTimeArray[i]}AM`);
+// formatting the hours in a work day
+for (let i = 0; i < timeArrayUnformatted.length; i++) {
+    if ( timeArrayUnformatted[i] < 6 || timeArrayUnformatted[i] == 12) {
+        timeArrayFormatted.push(`${timeArrayUnformatted[i]}PM`);
+    // } else if (timeArrayUnformatted[i] == 12) {
+    //     timeArrayFormatted.push(`${timeArrayUnformatted[i]} NOON`);
+    
+    }
+     else {
+        timeArrayFormatted.push(`${timeArrayUnformatted[i]}AM`);
     }
 }
 
+// Used for checking if there is any infromation in the local storage
 const localStorageGetter = () => {
     const currentStorage = localStorage.getItem(todayKey);
     if (currentStorage) {
@@ -40,58 +40,61 @@ const localStorageGetter = () => {
     }
 }
 
-const localStorageArray = [];
-
 var reachedCurrentTime = false;
-
 const container = $(".container");
+const currentStorage = localStorageGetter();
 
-for (let i = 0; i < finalTimerArray.length; i++) {
+for (let i = 0; i < timeArrayFormatted.length; i++) {
 
-    const currentStorage = localStorageGetter();
+    // Creating each row for each hour in a work day
+    const timeRow = $("<div class='row'></div>");
 
-    const toAppend = $("<div class='row'></div>");
+    const timeArea = $("<div class='col-1'></div>");
+    timeArea.text(timeArrayFormatted[i]);
 
-    const childTime = $("<div class='col-1'></div>");
-    childTime.text(finalTimerArray[i]);
-
-    childTime.addClass("d-flex align-items-center justify-content-center hour");
-    const testInput = $("<textarea class='col-10 description'></textarea");
+    timeArea.addClass("d-flex align-items-center justify-content-center hour");
+    const textArea = $("<textarea class='col-10 description'></textarea");
 
     if (currentStorage) {
         for (let j = 0; j < currentStorage.length; j++) {
-            if (Object.values(currentStorage[j])[0].includes(finalTimerArray[i])) {
-                testInput.text(Object.values(currentStorage[j])[1]);
+            // if (Object.values(currentStorage[j])[0].includes(timeArrayFormatted[i])) {
+            //     console.log(Object.values(currentStorage[j])[0]);
+            //     console.log(`\t${timeArrayFormatted[i]}`);
+            //     textArea.text(Object.values(currentStorage[j])[1]);
+            // }
+            // console.log(currentStorage[j].time);
+            // console.log*timeArrayFormatted[i]
+            if (currentStorage[j].time == timeArrayFormatted[i]) {
+                textArea.text(Object.values(currentStorage[j])[1]);
             }
         }
     }
 
-
     if (!reachedCurrentTime) {
-        testInput.addClass("past");
-        if ( newTimeArray[i] == hours) {
+        textArea.addClass("past");
+        if ( timeArrayUnformatted[i] == hours) {
         reachedCurrentTime = true;
-        testInput.removeClass("past");
-        testInput.addClass("present");
+        // Since nested in an if statement, it must be removed
+        // for the current time element
+        textArea.removeClass("past");
+        textArea.addClass("present");
         }
     } else if (!reachedCurrentTime == false) {
-        testInput.addClass("future");
+        textArea.addClass("future");
     } 
-
-
 
     const saveButtonCol = $("<div class='col-1'></div>");
     saveButtonCol.addClass("saveBtn");
     saveButtonCol.append("<i class='fas fa-save'></i>");
 
+    // Used for event bubbling on the icon
     $(".fas").on('click', () => {
     })
 
-
-
-
     // Set localstorage function
     saveButtonCol.on('click', (e) => {
+
+        // An if else to determine if the icon or field itself was clicked
         if ($(e.target).attr("class") == "fas fa-save") {
             var textTarget = $(e.target).parent().siblings(".col-10").val();
             var localKey = $(e.target).parent().siblings(".col-1").text();
@@ -102,19 +105,24 @@ for (let i = 0; i < finalTimerArray.length; i++) {
 
         }
 
-
         const currentStorageArray = localStorage.getItem(todayKey);
         let  parsedStorage = JSON.parse(currentStorageArray);
 
         const timeAndMessage = {time: localKey, "text": textTarget};
    
+        // Appender is used to make it where you can update the messages
+        // and have more than one text information stored in a day 
         appender = [];
         if (parsedStorage) {
             appender = parsedStorage;
         }
 
+        // Checks to see if there is a certain time value in an array 
+        // if there is then it will overwrite it instead of appending to the message 
         for (let i = 0; i < appender.length; i++) {
             if (Object.values(appender[i])[0].includes(timeAndMessage.time)) {
+                // console.log(timeAndMessage.time);
+                // console.log(Object.values(appender[i])[0]);
                 appender[i] = timeAndMessage;
                 localStorage.setItem(todayKey, JSON.stringify(appender));
                 return;
@@ -122,24 +130,16 @@ for (let i = 0; i < finalTimerArray.length; i++) {
         }
 
         appender.push(timeAndMessage);
-
         localStorage.setItem(todayKey, JSON.stringify(appender));
-
 
     });
 
-
-
-    toAppend.append(childTime);
-    toAppend.append(testInput);
-    toAppend.append(saveButtonCol);
-    container.append(toAppend);
+    // Adding all of the buttons to the DOM 
+    timeRow.append(timeArea);
+    timeRow.append(textArea);
+    timeRow.append(saveButtonCol);
+    container.append(timeRow);
 }
-// The code will be built dynamically 
-// There are 3 main things that you need, 
-// the Time, the input Text, and the save button. 
-// Event that have already happened are grey 
-// The current hour is red 
-// The future hours are green
+
 
 
